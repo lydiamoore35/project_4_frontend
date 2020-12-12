3e66e00ce40f22ffc4a2401841fc8984d1e734bd
Lydia Moore <lydiamoore35@gmail.com>
2020-12-11 21:58:43 -0700

    css is fine for now need to deploy

--- src/components/Checkin.js
+++ src/components/Checkin.js
@@ -3,10 +3,7 @@ import React from 'react'
 //need to use a form or form onSubmit??//
 function Checkin(props) {
     return (
-      <div className="checkin">
-        <h3>Check In</h3>
-            <p className="question">How are you feeling right now?</p>
-      </div>
+        <div className="checkin">Check In<br></br>How are you feeling right now?</div>
     )
 }
 
